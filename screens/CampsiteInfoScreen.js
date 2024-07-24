b6d2ca1ba87f656d93f6269716a75e59bc4606ed
Gabriella Andrade <gabii.marcelly@gmail.com>
2024-07-24 18:30:08 +0100

    Components Part 2

---
+++ screens/CampsiteInfoScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RenderCampsite from '../features/campsites/RenderCampsite';
+
+const CampsiteInfoScreen = (props) => {
+  return <RenderCampsite campsite={props.campsite} />;
+};
+
+export default CampsiteInfoScreen;
