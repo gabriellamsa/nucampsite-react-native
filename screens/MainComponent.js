63e1b714e16f3856d62eb660c8d7224d0471b7e4
Gabriella Andrade <gabii.marcelly@gmail.com>
2024-08-22 13:12:45 -0300

    Secure Store

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -21,6 +21,7 @@ import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import FavoritesScreen from './FavoritesScreen';
+import LoginScreen from './LoginScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -31,7 +32,6 @@ const screenOptions = {
 
 const HomeNavigator = () => {
   const Stack = createStackNavigator();
-
   return (
     <Stack.Navigator screenOptions={screenOptions}>
       <Stack.Screen
@@ -55,7 +55,6 @@ const HomeNavigator = () => {
 
 const AboutNavigator = () => {
   const Stack = createStackNavigator();
-
   return (
     <Stack.Navigator screenOptions={screenOptions}>
       <Stack.Screen
@@ -78,7 +77,6 @@ const AboutNavigator = () => {
 
 const ContactNavigator = () => {
   const Stack = createStackNavigator();
-
   return (
     <Stack.Navigator screenOptions={screenOptions}>
       <Stack.Screen
@@ -102,7 +100,6 @@ const ContactNavigator = () => {
 
 const ReservationNavigator = () => {
   const Stack = createStackNavigator();
-
   return (
     <Stack.Navigator screenOptions={screenOptions}>
       <Stack.Screen
@@ -126,7 +123,6 @@ const ReservationNavigator = () => {
 
 const FavoritesNavigator = () => {
   const Stack = createStackNavigator();
-
   return (
     <Stack.Navigator screenOptions={screenOptions}>
       <Stack.Screen
@@ -148,9 +144,30 @@ const FavoritesNavigator = () => {
   );
 };
 
+const LoginNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name='Login'
+        component={LoginScreen}
+        options={({ navigation }) => ({
+          headerLeft: () => (
+            <Icon
+              name='sign-in'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          )
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const DirectoryNavigator = () => {
   const Stack = createStackNavigator();
-
   return (
     <Stack.Navigator
       initialRouteName='Directory'
@@ -222,6 +239,21 @@ const Main = () => {
           drawerStyle: { backgroundColor: '#CEC8FF' }
         }}
       >
+        <Drawer.Screen
+          name='Login'
+          component={LoginNavigator}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Icon
+                name='sign-in'
+                type='font-awesome'
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            )
+          }}
+        />
         <Drawer.Screen
           name='Home'
           component={HomeNavigator}
